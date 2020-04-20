a816a5cc3f7e9cdc6baa19cb9cad0d54fba59cc8
ssKarim23 <60349198+ssKarim23@users.noreply.github.com>
2020-04-20 13:56:00 +0200

    Add files via upload

---
+++ Projet/main/articles-press.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  
+  <link rel="shortcut icon" href="src/img/favicon.ico">
+  <title>Articles & Press</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--Rana-->
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.8.0/css/bulma.min.css">
+  <link rel="stylesheet" type="text/css" href="../src/css/style.css">
+
+  <!--FONT-->
+  
+ 
+  <link href="https://fonts.googleapis.com/css2?family=Racing+Sans+One&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Cuprum:ital,wght@1,400;1,700&family=Patua+One&display=swap" rel="stylesheet">
+  <script src="https://kit.fontawesome.com/a076d05399.js" crossorigin="anonymous"></script>
+
+  <script src="https://kit.fontawesome.com/adbe6815a1.js" crossorigin="anonymous"></script>
+  
+  </head>
+</head>
+
+<body>
+
+
+<!--header-->
+<div class="block"> 
+  <header class="header">
+    
+    <a href="index.php" class="logo"><img src="../src/img/logo.png"></a>
+    <a href="index.php" class="header-logo"> NFL BET </a>
+    <input type="checkbox" id="check">
+      <label for="check" class="checkbtn">
+        <i class="fas fa-bars"></i>
+      </label>
+    
+    <nav class="header-menu">
+      <a href="index.php">Accueil</a>
+      <a href="articles-press.php"> Articles & Press</a>
+      <a href="../searchbar.php"> Rechercher</a>
+      <a href=""> Calendrier et résultats</a>
+      
+      <a href="../contacts/contacts.php"><button class="button is-link">Nous contactez</button></a>
+      
+    </nav>
+  </header>
+<!--header-->
+
+<!-- Articles & Press-->
+
+
+<div class="block">
+	<h1>Articles & Press</h1>
+	<div class="container">
+<div class="tile is-ancestor">
+  <div class="tile is-vertical is-8">
+    <div class="tile">
+      <div class="tile is-parent is-vertical">
+        <article class="tile is-child notification is-primary">
+          <p class="title"><a href="https://www.touchdownactu.com/234949/le-petit-dej-donald-trump-parle-dune-reprise-sans-supporters/">Donald Trump parle d’une reprise sans supporters</p></a>
+          <p class="subtitle">L’essentiel de l’actualité NFL</p>
+          <p class="subtitle-2">18 avril 2020 à 12h25</p>
+        </article>
+        <article class="tile is-child notification is-warning">
+          <p class="title"><a href="http://www.nfl.com/news/story/0ap3000001107981/article/2020-nfl-draft-ranking-each-position-group-in-this-years-class">Les dirigeants, scouts et entraîneurs de la NFL évaluent la classe QB: classer les 14 meilleurs</p></a>
+          <p class="subtitle">Une Analyse.</p>
+          <p class="subtitle-2">17 avril 2020 à 16h46</p>
+        </article>
+      </div>
+      <div class="tile is-parent">
+        <article class="tile is-child notification is-info">
+         <p class="title"><a href="https://www.touchdownactu.com/231880/fiches-draft-jalen-reagor-wr-le-pari-risque/">Jalen Reagor (WR), le pari risqué</p></a>
+         <p class="subtitle">Joueur gadget ou futur phénomène ? Jalen Reagor est une belle énigme de cette draft.</p>
+         <p class="subtitle-2">15 avril 2020 à 10h07</p>
+          <iframe width="420" height="400" src="https://www.youtube.com/embed/v9UQcG0ORMc" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+        </article>
+      </div>
+    </div>
+    <div class="tile is-parent">
+      <article class="tile is-child notification is-danger">
+        <p class="title"><a href="http://www.nfl.com/news/story/0ap3000001109661/article/2020-nfl-draft-prospectpro-comparisons-for-top-quarterbacks">Repêchage de la NFL 2020: Comparaisons Prospect-Pro pour les sept meilleurs quarterbacks</p></a>
+          <p class="subtitle">Une Analyse.</p>
+          <p class="subtitle-2">15 avril 2020 à 12h15</p>
+        <div class="content">
+          <!-- Content -->
+        </div>
+      </article>
+    </div>
+  </div>
+  <div class="tile is-parent">
+    <article class="tile is-child notification is-success">
+      <div class="content">
+         <p class="title"><a href="https://www.touchdownactu.com/234966/leonard-fournette-aimerait-que-les-jaguars-signent-cam-newton/">Leonard Fournette aimerait que les Jaguars signent Cam Newton</p></a>
+          <p class="subtitle">Gardner Minshew doit apprécier le soutien sans faille de son coéquipier.</p>
+          <p class="subtitle-2">13 avril 2020 à 18h23</p>
+        <figure class="image is-9by6">
+            <img src="../src/img/1.jpg">
+          </figure>
+        <div class="content">
+          <!-- Content -->
+        </div>
+      </div>
+    </article>
+  </div>
+</div>
+
+<!-- Articles & Press-->
+
+
+
+<!-- footer-->
+<div class="block">
+  <footer class="footer">
+    <div class="footer-information">
+      <p>Route de Mende, 34090 Montpellier</p>
+      <p>00 33 66 93 06 82 0</p>
+    </div> 
+  </footer>
+</div>
+<!-- footer-->
+
+
+
+<!--JS-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="src/js/main.js"></script>
+
+</body>
+</html>
